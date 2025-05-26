f0876e5fe9216a98f410ab2e067ab59ae3dcf67d
zeynOQ85 <maryamfarhadi1847@gmail.com>
2025-05-26 10:17:29 +0330

    class playlist comit

--- main.cpp
+++ main.cpp
@@ -23,3 +23,16 @@ public:
 	string getStyle() { return style; }
 	int getReleazeYear() { int releazeYear; }
 };
+class Song :public Entity {
+	string singer;
+	int releazeYear;
+	string style;
+public:
+	Song(string name, string id, string singer, int releazeYear, string style) :Entity(name, id), singer(singer), releazeYear(releazeYear), style(style) {}
+	void display() {
+		cout << "Song naame:" << name << "| singer:" << singer << "| releaze year:" << releazeYear << "| Song style:" << style << endl;
+	}
+	string getSinger() { return singer; }
+	string getStyle() { return style; }
+	int getReleazeYear() { int releazeYear; }
+};
